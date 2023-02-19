80519d529546a967538a3f26715099fc8d867513
criscabe <55502955+criscabe@users.noreply.github.com>
2023-02-19 18:04:43 +0100

    Reduce CIRCE raw images

--- code/Data_reduction/reduce.py
+++ code/Data_reduction/reduce.py
@@ -167,4 +167,3 @@ if __name__ == "__main__":
             outfile = infile[:-5] + "_coadd.fits"
             list_object_coadd.append(outfile)
             read_raw_circe(infile, nramps, outfile, debug=1)
-
